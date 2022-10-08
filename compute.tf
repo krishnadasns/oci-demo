bee1d5b480cb7a69526ce4115e4ca331a30aba2d
Krishnadas <krishnadas.ns@adfolks.com>
2022-10-08 23:57:45 +0400

    provider and cmp edited

--- compute.tf
+++ compute.tf
@@ -1,7 +1,7 @@
 resource "oci_core_instance" "ubuntu_instance" {
     # Required
     availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
-    compartment_id = "ocid1.compartment.oc1..aaaaaaaaw4jdj47nyhxy3oksi2uzxh3mjtxla7teqsqjytdtd3qkcniczm2a"
+    compartment_id = "ocid1.tenancy.oc1..aaaaaaaa5kd25axph5zakcagxyepimrfujo6inw63mmt6mvwlcnozhhuwcfq"
     shape = "VM.Standard2.1"
     source_details {
         source_id = "ocid1.image.oc1.me-dubai-1.aaaaaaaaajejnzkrk3fodof5chtoldhnpozleq2wmk5abi5un3trijkxd3qa"
--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "oci" {
-  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaa5kd25axph5zakcagxyepimrfujo6inw63mmt6mvwlcnozhhuwcfq"
-  user_ocid = "ocid1.user.oc1..aaaaaaaamg2zw77cb5zlfh35tfqtwhmrcixj6wlf6t6qyivnjlruuyhxmota" 
-  private_key_path = "/home/krishnadas/.oci/jasimocirsakey.pem"
-  fingerprint = "ee:51:80:b4:7e:87:99:91:b1:09:5f:96:63:48:75:f7"
+  # tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaa5kd25axph5zakcagxyepimrfujo6inw63mmt6mvwlcnozhhuwcfq"
+  # user_ocid = "ocid1.user.oc1..aaaaaaaamg2zw77cb5zlfh35tfqtwhmrcixj6wlf6t6qyivnjlruuyhxmota" 
+  # private_key_path = "/home/krishnadas/.oci/jasimocirsakey.pem"
+  # fingerprint = "ee:51:80:b4:7e:87:99:91:b1:09:5f:96:63:48:75:f7"
   region = "me-dubai-1"
 }
